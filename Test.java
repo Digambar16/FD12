7d5c3b56853cf3eaaf714a29c12ebc82a95e78b6
Digambar16 <digambarwaybhase@gmail.com>
2023-02-11 19:17:15 +0530

    Successfully Completed v1

--- Test.java
+++ Test.java
@@ -5,5 +5,11 @@ public static void main(String args[])
 {
 
 System.out.println("Welcome To CJC");
+
+System.out.println("Welcome To Sinhgad College of Engineering");
+
+System.out.println("Welcome To Ahmednagar");
+
+System.out.println("Welcome To Akurdi");
 }
 }
